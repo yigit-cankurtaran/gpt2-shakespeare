32f3dc59fbcfce80882ee75f833804a0c040cce1
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-09-10 08:52:24 +0300

    train-test-val split

--- first.py
+++ first.py
@@ -1,10 +1,17 @@
 from datasets import load_dataset
 
-dataset = load_dataset("text", data_files="./tinyshakespeare.txt")
+load_data = load_dataset("text", data_files="./tinyshakespeare.txt")
 print("dataset created successfully")
 
-split_dataset = dataset["train"].train_test_split(test_size=0.2)
-train_dataset = split_dataset["train"]
-test_dataset = split_dataset["test"]
-print(train_dataset)
-print(test_dataset)
+# first split train-test, train_val["test"] will be test
+# second split train-val split, 80% of data, train_test["train"] will be train and train_test["test"] will be val
+train_val = load_data["train"].train_test_split(test_size=0.2, seed=0) # 80% train 20% test
+train_test = train_val["train"].train_test_split(test_size=0.125, seed=0) # validation 10%, 12.5% of train
+
+dataset = {
+    "train": train_test["train"],
+    "validation": train_test["test"],
+    "test": train_val["test"]
+}
+
+print(dataset)
